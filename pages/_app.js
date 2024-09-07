d8af507517aeb7e8cc57459fcf29d975e5fffd22
Rick <rickvdkommer@gmail.com>
2024-09-07 13:00:57 +0200

    i saw you

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import { useGoogleAnalytics } from '../hooks/useGoogleAnalytics'; // Import the hook
+import { useGoogleAnalytics } from 'pages/hooks/useGoogleAnalytics.js'; // Import the hook
 
 const App = ({ Component, pageProps }) => {
   useGoogleAnalytics(); // Use the Google Analytics hook
