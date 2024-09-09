525c3a1461987c2b84945c506b9448a5c071f5f9
Rick <rickvdkommer@gmail.com>
2024-09-09 19:43:50 +0200

    try again tag

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import Button from "../components/Button";
 import Link from "next/link";
 import Cursor from "../components/Cursor";
 import { Analytics } from "@vercel/analytics/react"
+import { useEffect, Fragment } from 'react';
 
 // Local Data
 import data from "../data/portfolio.json";
@@ -52,17 +53,9 @@ export default function Home() {
   return (
     <div className="relative cursor-none">
       <Cursor />
-      <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z3VTDKXJ7Z"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-Z3VTDKXJ7Z');
-      </script>
-        <title>Clic Media - Creative Videography & Photography Agency based in Amsterdam</title>
-        <meta name="description" content="Enhance your brand with our professional videography and photography services in Amsterdam. We offer a unique, result-driven approach to capturing stunning visuals for businesses, events, and corporate projects. Partner with our creative Amsterdam-based team for high-quality videos and images tailored to your needs." />
+      <Head>        
+      <title>Clic Media - Creative Videography & Photography Agency based in Amsterdam</title>
+      <meta name="description" content="Enhance your brand with our professional videography and photography services in Amsterdam. We offer a unique, result-driven approach to capturing stunning visuals for businesses, events, and corporate projects. Partner with our creative Amsterdam-based team for high-quality videos and images tailored to your needs." />
 
       </Head>
       
