d05ed8509d0a69fa08c535143c4be8c1d120dad2
Rick <rickvdkommer@gmail.com>
2024-09-15 15:08:34 +0200

    back

--- components/portfolio/portfolio.js
+++ components/portfolio/portfolio.js
@@ -150,7 +150,7 @@ const TabbedPortfolio = ({ projects = [], projectType }) => {
 
             {filteredProjects[activeTab]?.media?.length > 0 && (
               <div className="flex justify-center w-full">
-                <div className="w-full sm:w-2/5 rounded-lg shadow-md">
+                <div className="sm:w-2/5 rounded-lg shadow-md">
                   <Splide
                     className="rounded-lg shadow-md"
                     options={{
