340741914bbb28b744be79e4c8328b88ad0bd12a
Rick <rickvdkommer@gmail.com>
2024-10-14 14:07:50 +0200

    fixscrollreset

--- components/LogoSlider/logoslider.js
+++ components/LogoSlider/logoslider.js
@@ -1,39 +1,19 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import styles from './LogoSlider.module.css';
 
 const LogoSlider = ({ logos }) => {
-  const sliderRef = useRef(null);
-
-  useEffect(() => {
-    const slider = sliderRef.current;
-    let animationId;
-
-    const animate = () => {
-      if (slider.scrollLeft >= slider.scrollWidth / 2) {
-        slider.scrollLeft = 0;
-      } else {
-        slider.scrollLeft += 1;
-      }
-      animationId = requestAnimationFrame(animate);
-    };
-
-    animationId = requestAnimationFrame(animate);
-
-    return () => cancelAnimationFrame(animationId);
-  }, []);
-
   return (
     <div className={styles.logoSliderContainer}>
-      <div className={styles.logoSlider} ref={sliderRef}>
+      <div className={styles.logoSlider}>
         {logos.concat(logos).map((logo, index) => (
           <div key={index} className={styles.logoWrapper}>
             <Image
-            src={logo.src}
-            alt={logo.alt}
-            width={200} // Increased from 100
-            height={120} // Increased from 50, maintaining aspect ratio
-            className={styles.logo}
+              src={logo.src}
+              alt={logo.alt}
+              width={180}
+              height={90}
+              className={styles.logo}
             />
           </div>
         ))}
