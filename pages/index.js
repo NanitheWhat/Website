2df38d81c025d96e7fcdf6181059b4168fd88352
rickmacbook <rickkomkommer@gmail.com>
2024-10-30 20:32:07 +0100

    change title + colors

--- pages/index.js
+++ pages/index.js
@@ -87,30 +87,43 @@ export default function Home({ logos }) {
   
   <div className="flex flex-col items-center w-full">
     <div className="text-center max-w-4xl">
-      <h1
-        ref={textOne}
-        className="text-3xl tablet:text-5xl laptop:text-5xl laptopl:text-6xl p-2 tablet:p-4 font-extrabold leading-relaxed transition-opacity duration-500 ease-in-out"
-      >
-        {data.headerTaglineOne}
-      </h1>
-      <h2
-        ref={textTwo}
-        className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed transition-opacity duration-500 ease-in-out"
-      >
-        {data.headerTaglineTwo}
-      </h2>
-      <h3
-        ref={textThree}
-        className="text-xl tablet:text-3xl laptop:text-3xl laptopl:text-4xl p-2 tablet:p-4 font-semibold leading-relaxed transition-opacity duration-500 ease-in-out"
-      >
-        {data.headerTaglineThree}
-      </h3>
-      <h4
-        ref={textFour}
-        className="text-lg tablet:text-2xl laptop:text-2xl laptopl:text-3xl p-2 tablet:p-4 font-medium leading-relaxed transition-opacity duration-500 ease-in-out"
-      >
-        {data.headerTaglineFour}
-      </h4>
+    <h1
+      ref={textOne}
+      className="text-3xl tablet:text-5xl laptop:text-5xl laptopl:text-6xl p-2 tablet:p-4 font-extrabold leading-relaxed transition-opacity duration-500 ease-in-out"
+    >
+      We create{" "}
+      <span className="bg-gradient-green text-transparent bg-clip-text">
+        result-driven
+      </span>{" "}
+      <span className="bg-gradient-orange text-transparent bg-clip-text">
+        social-ads
+      </span>
+    </h1>
+    <h2
+      ref={textTwo}
+      className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed transition-opacity duration-500 ease-in-out"
+    >
+      {/* You can add content here if needed */}
+    </h2>
+    <h3
+      ref={textThree}
+      className="text-xl tablet:text-3xl laptop:text-3xl laptopl:text-4xl p-2 tablet:p-4 font-semibold leading-relaxed transition-opacity duration-500 ease-in-out"
+    >
+      So you can{" "}
+      <span className="bg-gradient-green text-transparent bg-clip-text">
+        focus
+      </span>{" "}
+      on running{" "}
+      <span className="bg-gradient-orange text-transparent bg-clip-text">
+        your business
+      </span>
+    </h3>
+    <h4
+      ref={textFour}
+      className="text-lg tablet:text-2xl laptop:text-2xl laptopl:text-3xl p-2 tablet:p-4 font-medium leading-relaxed transition-opacity duration-500 ease-in-out"
+    >
+      {/* You can add content here if needed */}
+    </h4>
     </div>
 
     <div className="mt-6 w-full flex justify-center">
@@ -146,7 +159,7 @@ export default function Home({ logos }) {
         </div>
 
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
-          <h2 className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed w-full laptop:w-4/5 transition-opacity duration-500 ease-in-out">Services.</h2>
+          <h2 className="bg-gradient-orange text-transparent bg-clip-text text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed w-full laptop:w-4/5 transition-opacity duration-500 ease-in-out">Services.</h2>
           <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
             {data.services.map((service, index) => (
               <ServiceCard
@@ -167,7 +180,7 @@ export default function Home({ logos }) {
         )}
 
         <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
-          <h2 className="tablet:m-10 text-2xl text-bold">About.</h2>
+          <h2 className="bg-gradient-green text-transparent bg-clip-text text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed w-full laptop:w-4/5 transition-opacity duration-500 ease-in-out">About.</h2>
           <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
             {data.aboutpara}
           </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,32 @@ module.exports = {
       desktop: "1280px",
       laptopl: "1440px",
     },
-    extend: {},
+    
+    extend: {
+      backgroundImage: {
+        'gradient-orange': 'linear-gradient(to right,#FF5F1F, #FFAC1C)',
+        'gradient-green': 'linear-gradient(to right, #355E3B, #228B22)',
+      },
+      fontFamily: {
+        
+        sans: [
+          'Inter var, sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
   },
   plugins: [],
 };
