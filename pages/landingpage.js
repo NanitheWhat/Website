4713da6ad6833f286749a5946033c582882e711c
rickmacbook <rickkomkommer@gmail.com>
2024-12-19 19:16:18 +0100

    containerpage

--- pages/landingpage.js
+++ pages/landingpage.js
@@ -38,7 +38,7 @@ export default function LandingPage({ logos }) {
     
     <div className="relative overflow-x-hidden overflow-y-hidden">
         <Cursor />
-        <div className="container mx-auto mb-10"></div>
+        
         <Head>
             <title>Clicmedia - Landing Page</title>
             <meta
@@ -48,78 +48,77 @@ export default function LandingPage({ logos }) {
         </Head>
         <div className="gradient-circle"></div>
         <div className="gradient-circle-bottom"></div>
+        <div className="container mx-auto mb-10"></div>
 
-        <Header/>
+          <Header/>
       
 
-      {/* Video Section */}
-      <section className="container mx-auto my-10 p-4">
-        <h1 className="text-center text-3xl tablet:text-5xl pb-20 laptop:text-6xl font-extrabold mt-5">
-          Wij maken{" "}
-          <span className="bg-gradient-green text-transparent bg-clip-text">CONTENT</span>{" "}
-          dat{" "}
-          <span className="bg-gradient-orange text-transparent bg-clip-text">CONVERTEERT!</span>
-          
-        </h1>
-        <div className="relative w-full h-0 pb-[56.25%]">
-          <iframe
-            className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/sbSP9aiCOBY?autoplay=1&controls=0&mute=0&playsinline=1&rel=0&modestbranding=1"
-            title="Clicmedia Introduction Video"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <section className="my-10 flex flex-col justify-center items-center">
-        <h2 className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed text-center">
-            Kijken wat wij voor jou kunnen betekenen?
-        </h2>
-        <Link href="https://calendly.com/rickvandenkommer/clic-discovery">
-            <Button2 className="mt-10 mx-auto text-lg tablet:text-2xl laptop:text-3xl px-10 py-4">
-            Schedule nu een meeting!
-            </Button2>
-        </Link>
-        </section>
+            {/* Video Section */}
+            <section className="container mx-auto my-10 p-4">
+              <h1 className="text-center text-3xl tablet:text-5xl pb-20 laptop:text-6xl font-extrabold mt-5">
+                Wij maken{" "}
+                <span className="bg-gradient-green text-transparent bg-clip-text">CONTENT</span>{" "}
+                dat{" "}
+                <span className="bg-gradient-orange text-transparent bg-clip-text">CONVERTEERT!</span>
+                
+              </h1>
+              <div className="relative w-full h-0 pb-[56.25%]">
+                <iframe
+                  className="absolute top-0 left-0 w-full h-full"
+                  src="https://www.youtube.com/embed/sbSP9aiCOBY?autoplay=1&controls=0&mute=0&playsinline=1&rel=0&modestbranding=1"
+                  title="Clicmedia Introduction Video"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+              <section className="my-10 flex flex-col justify-center items-center">
+                <h2 className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed text-center">
+                    Kijken wat wij voor jou kunnen betekenen?
+                </h2>
+                <Link href="https://calendly.com/rickvandenkommer/clic-discovery">
+                    <Button2 className="mt-10 mx-auto text-lg tablet:text-2xl laptop:text-3xl px-10 py-4">
+                    Schedule nu een meeting!
+                    </Button2>
+                </Link>
+              
 
-      </section>
+            </section>
+            <section ref={logoRef} className="my-10 ">
+              <h2 className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed text-center mb-20">
+              <span className="bg-gradient-orange text-transparent bg-clip-text">Bedrijven</span>{" "} waar wij mee hebben
+              {" "}<span className="bg-gradient-green text-transparent bg-clip-text">gewerkt.</span>{" "}
+              </h2>
+              <LogoSlider logos={logos} className="pt-20 "/>
+            </section>
 
-
-      {/* Logo Slider */}
-      <section ref={logoRef} className="my-10 ">
-        <h2 className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed text-center mb-20">
-        <span className="bg-gradient-orange text-transparent bg-clip-text">Bedrijven</span>{" "} waar wij mee hebben
-        {" "}<span className="bg-gradient-green text-transparent bg-clip-text">gewerkt.</span>{" "}
-        </h2>
-        <LogoSlider logos={logos} className="pt-20 "/>
-      </section>
-
-      {/* Image Section */}
-      <section className="my-20 flex justify-center items-center p-4">
-            <Image
-                src="/images/Frame 1 (3).png" // Use the actual path to the image you uploaded
-                alt="Sales Prospecting Services"
-                width={1344} // 70% of 1920
-                height={756} // 70% of 1080
-                className="mx-auto"
-                style={{ width: "70%", height: "auto" }} // reduce to 70% width
-            />
-        </section>
+            {/* Image Section */}
+            <section className="my-20 flex justify-center items-center p-4">
+                  <Image
+                      src="/images/Frame 1 (3).png" // Use the actual path to the image you uploaded
+                      alt="Sales Prospecting Services"
+                      width={1344} // 70% of 1920
+                      height={756} // 70% of 1080
+                      className="mx-auto"
+                      style={{ width: "70%", height: "auto" }} // reduce to 70% width
+                  />
+            </section>
 
 
-      {/* Call to Action Button */}
-      <section className="my-10 flex flex-col justify-center items-center">
-        <h2 className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed text-center">
-          Kijken wat wij voor jou kunnen betekenen?
-        </h2>
-        <Link href="https://calendly.com/rickvandenkommer/clic-discovery">
-            <Button2 className="mt-10 mx-auto text-lg tablet:text-2xl laptop:text-3xl px-10 py-4">
-            Zet een meeting op, of stuur een appje!
-            </Button2>
-        </Link>
-        </section>
+            {/* Call to Action Button */}
+            <section className="my-10 flex flex-col justify-center items-center">
+              <h2 className="text-2xl tablet:text-4xl laptop:text-4xl laptopl:text-5xl p-2 tablet:p-4 font-bold leading-relaxed text-center">
+                Kijken wat wij voor jou kunnen betekenen?
+              </h2>
+              <Link href="https://calendly.com/rickvandenkommer/clic-discovery">
+                  <Button2 className="mt-10 mx-auto text-lg tablet:text-2xl laptop:text-3xl px-10 py-4">
+                  Zet een meeting op, of stuur een appje!
+                  </Button2>
+              </Link>
+            </section>
+            </section>
 
-      <Footer />
+          <Footer />
       <div className="container mx-auto mb-10"></div>
     </div>
   );
